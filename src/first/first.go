2392a2c3c179fa379e504319a4e7fd2734e2c3d9
叶朝阳 <ycyoes@xiezhaoyangdeMacBook-Pro.local>
2020-01-02 14:58:20 +0800

    函数式编程

--- src/first/first.go
+++ src/first/first.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-type iAdder func(int) (int iAdder)
+type iAdder func(int) (int, iAdder)
 
 func main() {
 	fmt.Println("Hello World!")
@@ -13,6 +13,22 @@ func main() {
 	a := adder()
 	for i := 0; i < 10; i++ {
 		fmt.Printf("0 + 1 + 2 + ... + %d = %d\n", i, a(i))
+	}
+
+	fmt.Println("---函数式编程---")
+
+	b := adderV2(0)
+	fmt.Print(b)
+	for i := 0; i < 10; i++ {
+		var s int
+		s, b = b(i)
+		fmt.Printf("0 + 1 + 2 + ... + %d = %d\n", i, s)
+	}
+}
+
+func adderV2(base int) iAdder {
+	return func(v int) (int, iAdder) {
+		return base + v, adderV2(base + v)
 	}
 }
 
