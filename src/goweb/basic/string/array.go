830ad0ace01fd40deda25db530bd655980f8c52b
叶朝阳 <ycyoes@xiezhaoyangdeMacBook-Pro.local>
2020-01-03 15:34:47 +0800

    use operation

--- src/goweb/basic/string/array.go
+++ src/goweb/basic/string/array.go
@@ -14,4 +14,25 @@ func main() {
 	fmt.Println(b)
 	fmt.Printf("The first element is %d\n", arr[0])
 	fmt.Printf("The last element is %d\n", arr[9])
+
+	sum := 0
+	for index := 0; index < 10; index++ {
+		sum += index
+	}
+	fmt.Printf("sum is equal to %d", sum)
+
+	//for可简写为：
+	sum2 := 1
+	for sum2 < 1000 {
+		sum2 += sum2
+	}
+	fmt.Println("sum2: ", sum2)
+
+	//进一步简化
+	sum3 := 1
+	for sum3 < 1000 {
+		sum3 += sum3
+	}
+	fmt.Println("sum3 ", sum3)
+
 }
