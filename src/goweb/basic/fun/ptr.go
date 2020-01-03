9fbbf701f85ec909280bf2e93935a7a5d0d52461
ycyoes <ycyoes@163.com>
2020-01-03 15:58:10 +0800

    传指针

--- src/goweb/basic/fun/ptr.go
+++ src/goweb/basic/fun/ptr.go
@@ -10,8 +10,21 @@ func main() {
 	x1 := add1(x)
 	fmt.Println("x+1 = ", x1)
 	fmt.Println("x = ", x)
+
+	y := 3
+	fmt.Println("y = ", y)
+	y1 := add2(&y)
+	fmt.Println("y+1 = ", y1)
+	fmt.Println("y = ", y)
 }
 
+//传指针
+func add2(a *int) int {
+	*a = *a + 1 //修改了a的值
+	return *a
+}
+
+//传值
 func add1(a int) int {
 	a = a + 1
 	return a
