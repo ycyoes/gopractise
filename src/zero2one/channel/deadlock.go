61ce2f01c1edfaa49bdb7ca588d38479bd44bf5d
ycyoes <ycyoes@163.com>
2020-07-08 14:43:37 +0800

    deadlock

--- src/zero2one/channel/deadlock.go
+++ src/zero2one/channel/deadlock.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main()  {
 	pipline := make(chan string)
+	go hello(pipline)
+	
 	pipline <- "hello world"
-	fmt.Println(<-pipline)
+	ele := <- pipline
+	fmt.Printf("ele data %v\n",  ele)
 	close(pipline)
+}
+
+func hello(pipline chan string)  {
+	<-pipline
 }
