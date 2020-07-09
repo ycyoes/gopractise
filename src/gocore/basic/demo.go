4d9a06e31562127e619690aeafd44b259a4fb035
ycyoes <ycyoes@163.com>
2020-07-09 13:27:48 +0800

    demo

---
+++ src/gocore/basic/demo.go
@@ -0,0 +1,18 @@
+// demo.go
+
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var name = getTheFlag()
+	flag.Parse()
+	fmt.Printf("Hello, %v!\n", *name)
+}
+
+func getTheFlag() *string {
+	return flag.String("name", "everyone", "The greeting object.")
+}
