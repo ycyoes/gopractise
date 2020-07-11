154949a988db546ee19fa5b75948fabbc4fb80a2
ycyoes <ycyoes@163.com>
2020-07-11 23:43:41 +0800

    go type

--- src/rudiments/chapter2/typetest.go
+++ src/rudiments/chapter2/typetest.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 )
 
 func main() {
@@ -11,8 +13,36 @@ func main() {
 	fmt.Println(b)
 	b = true
 	fmt.Println(b)
+
+	var s string = "string"
+	var i int = 10
+	var f float32 = 1.2
+
+	fmt.Println(reflect.TypeOf(s))
+	fmt.Println(reflect.TypeOf(i))
+	fmt.Println(reflect.TypeOf(f))
+
+	s = "true"
+	b, err := strconv.ParseBool(s)
+	fmt.Println(b, err)
+
+	s = strconv.FormatBool(true)
+	fmt.Println(s)
+
+	fmt.Println(str2int("2d"))
 }
 
 func sayHello(s string) string {
 	return "Hello, " + s
+}
+
+//字符串类型转int
+func str2int(s string) int {
+	// r, err := strconv.ParseInt(s)
+	r, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+		fmt.Println(err)
+	}
+	return r
 }
