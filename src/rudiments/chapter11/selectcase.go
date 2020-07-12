ed2309945f95ba93b3e41a090b82bc636cc4e413
ycyoes <ycyoes@163.com>
2020-07-12 22:40:57 +0800

    select

---
+++ src/rudiments/chapter11/selectcase.go
@@ -0,0 +1,33 @@
+// selectcase
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	go ping1(channel1)
+	go ping2(channel2)
+
+	select {
+	case msg1 := <-channel1:
+		fmt.Println("received", msg1)
+	case msg2 := <-channel2:
+		fmt.Println("received", msg2)
+	case <-time.After(500 * time.Millisecond):
+		fmt.Println("no messages received. giving up.")
+	}
+}
+
+func ping1(c chan string) {
+	time.Sleep(time.Second * 3)
+	c <- "ping on channel1"
+}
+
+func ping2(c chan string) {
+	time.Sleep(time.Second * 2)
+	c <- "ping on channel2"
+}
