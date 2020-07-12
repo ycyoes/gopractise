bd7f6a971fdc4f48fe796e003d92d25054db47f2
ycyoes <ycyoes@163.com>
2020-07-12 08:47:37 +0800

    code block

---
+++ src/rudiments/chapter3/vartest.go
@@ -0,0 +1,28 @@
+// vartest
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	var f float32
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	if s == "" {
+		fmt.Printf("s has not been assigned a value and is zero valued.\n")
+	}
+
+	s = "Hello world"
+	fmt.Printf("Print 's' variable from outer block %v\n", s)
+	b = true
+	if b {
+		fmt.Printf("Printing 'b' variable from outer block %v\n", b)
+		i := 42
+		if b != false {
+			fmt.Printf("Printing 'i' variable from outer block %v\n", i)
+		}
+	}
+}
