0bc7128e05d868c53a552dbd971d97595c6c9eb7
ycyoes <ycyoes@163.com>
2020-07-12 16:55:10 +0800

    error test

--- src/rudiments/chapter10/errortest.go
+++ src/rudiments/chapter10/errortest.go
@@ -13,4 +13,24 @@ func main() {
 		return
 	}
 	fmt.Println("%s", file)
+
+	name, role := "Richard Jupp", "Drummer"
+	err = fmt.Errorf("The %v %v quit", role, name)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	n, err := Half(19)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(n)
+}
+
+func Half(numberToHalf int) (int, error) {
+	if numberToHalf%2 != 0 {
+		return -1, fmt.Errorf("Cannot half %v", numberToHalf)
+	}
+	return numberToHalf / 2, nil
 }
