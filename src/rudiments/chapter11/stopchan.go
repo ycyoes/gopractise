b630825322a59623af62f40e0a75251afb6236c6
ycyoes <ycyoes@163.com>
2020-07-12 19:56:12 +0800

    stop channel

---
+++ src/rudiments/chapter11/stopchan.go
@@ -0,0 +1,35 @@
+// stopchan
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	messages := make(chan string)
+	stop := make(chan bool)
+	go sender(messages)
+	go func() {
+		time.Sleep(time.Second * 2)
+		fmt.Println("Time's up!")
+		stop <- true
+	}()
+
+	for {
+		select {
+		case <-stop:
+			return
+		case msg := <-messages:
+			fmt.Println(msg)
+		}
+	}
+}
+
+func sender(c chan string) {
+	t := time.NewTicker(1 * time.Second)
+	for {
+		c <- "I'm sending a message"
+		<-t.C
+	}
+}
