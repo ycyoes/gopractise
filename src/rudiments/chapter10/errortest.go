a4855c4489eb8ac75ed52d5eab92ef4845bfba1b
ycyoes <ycyoes@163.com>
2020-07-12 16:44:21 +0800

    error

---
+++ src/rudiments/chapter10/errortest.go
@@ -0,0 +1,16 @@
+// errortest
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	file, err := ioutil.ReadFile("foo.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("%s", file)
+}
