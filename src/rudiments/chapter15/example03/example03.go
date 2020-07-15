288ecfb57b7c947f15316975029922479e3a1e86
ycyoes <ycyoes@163.com>
2020-07-15 08:21:57 +0800

    test

---
+++ src/rudiments/chapter15/example03/example03.go
@@ -0,0 +1,20 @@
+// example03
+package example03
+
+func translate(s string) string {
+	switch s {
+	case "en-US":
+		return "Hello"
+	case "fr-FR":
+		return "Bonjour"
+	case "it-IT":
+		return "Ciao"
+	default:
+		return "Hello"
+	}
+}
+
+func Greeting(name, locale string) string {
+	salutation := translate(locale)
+	return (salutation + name)
+}
