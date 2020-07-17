b1073394c9d1eefed5b51ed2b19233e0972a3c3e
ycyoes <ycyoes@163.com>
2020-07-18 01:14:37 +0800

    string int bool symbol

--- src/rudiments/chapter17/flagtest.go
+++ src/rudiments/chapter17/flagtest.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 	s := flag.String("s", "Hello world", "String help text")
+	i := flag.Int("i", 1, "Int help text")
+	b := flag.Bool("b", false, "Bool help text")
 	flag.Parse()
 	fmt.Println("value of s: ", *s)
+	fmt.Println("value of i:", *i)
+	fmt.Println("value of b:", *b)
 }
