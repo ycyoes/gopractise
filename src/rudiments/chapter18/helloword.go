78e433c58970e84d5f5be0a6be2bd5bf3811cf05
ycyoes <ycyoes@163.com>
2020-07-25 19:10:41 +0800

    handle 404

--- src/rudiments/chapter18/helloword.go
+++ src/rudiments/chapter18/helloword.go
@@ -7,9 +7,13 @@ import (
 
 func main() {
 	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8001", nil)
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello World\n"))
 }
