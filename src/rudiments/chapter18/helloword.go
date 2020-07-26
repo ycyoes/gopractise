2811593c31c8a982d0265647add90a314220c131
ycyoes <ycyoes@163.com>
2020-07-26 13:11:09 +0800

    accept

--- src/rudiments/chapter18/helloword.go
+++ src/rudiments/chapter18/helloword.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(":8001", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +15,15 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json;charset=utf-8")
-	w.Write([]byte("Hello World\n"))
+	switch r.Header.Get("Accept") {
+	case "application/json":
+		w.Header().Set("Content-Type", "application/json;charset=utf-8")
+		w.Write([]byte("Hello World\n"))
+	case "application/xml":
+		w.Header().Set("Content-Type", "applicaton/xml;charset=utf-8")
+		w.Write([]byte(`<?xml version="1.0" encoding="utf-8"?><Message>Hello World</Message>`))
+	default:
+		w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+		w.Write([]byte("Hello World\n text/plain"))
+	}
 }
