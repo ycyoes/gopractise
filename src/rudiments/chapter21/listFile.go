2b0289abfea5d7cb2ebe278f920f742de479d87c
ycyoes <ycyoes@163.com>
2020-08-01 17:29:36 +0800

    list dir

---
+++ src/rudiments/chapter21/listFile.go
@@ -0,0 +1,19 @@
+// listFile
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Mode(), " ", file.Name())
+	}
+}
