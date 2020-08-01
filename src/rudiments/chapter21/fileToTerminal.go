41e1093594245812b3215dfc1a8c754eb0509c90
ycyoes <ycyoes@163.com>
2020-08-01 16:59:05 +0800

    read file

---
+++ src/rudiments/chapter21/fileToTerminal.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	fileBytes, err := ioutil.ReadFile("example01.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(fileBytes)
+
+	fileString := string(fileBytes)
+	fmt.Println(fileString)
+}
