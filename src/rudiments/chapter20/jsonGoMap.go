521bf890165932f119dc9f6fd9ee3e256569b82e
ycyoes <ycyoes@163.com>
2020-08-01 16:35:51 +0800

    json go map

---
+++ src/rudiments/chapter20/jsonGoMap.go
@@ -0,0 +1,23 @@
+// jsonGoMap
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Switch struct {
+	On bool `json:"on"`
+}
+
+func main() {
+	jsonStringData := `{"on":"true"}`
+	jsonByteData := []byte(jsonStringData)
+	s := Switch{}
+	err := json.Unmarshal(jsonByteData, &s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("%+v\n", s)
+}
