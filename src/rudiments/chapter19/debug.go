6bffd05173ac5377d361680ba4a11d0ecf977118
ycyoes <ycyoes@163.com>
2020-08-01 15:45:08 +0800

    timeout

--- src/rudiments/chapter19/debug.go
+++ src/rudiments/chapter19/debug.go
@@ -5,14 +5,35 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"time"
 )
 
 func main() {
 	debug := os.Getenv("DEBUG")
-	client := &http.Client{}
+
+	tr := &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		TLSHandshakeTimeout:   10 * time.Second,
+		IdleConnTimeout:       90 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
+	/**
+	* 通过创建一个传输（transport）并将其传递给客户端，可更细致地控制超时：控制HTTP连接的各个阶段。
+	* 在大多数情况下，使用Timeout就足以控制整个HTTP事务，但在Go语言中，还可通过创建传输来控制HTTP事务的各个部分。
+	**/
+	client := &http.Client{
+		// Timeout: 50 * time.Millisecond,
+		Transport: tr,
+	}
 	request, err := http.NewRequest("GET", "https://ifconfig.co", nil)
 	//设置返回信息的格式
 	request.Header.Add("Accept", "application/json")
