755cc633eb058454deeac73064eabec9a0f32a74
ycyoes <ycyoes@163.com>
2020-08-02 10:39:31 +0800

    folder

---
+++ src/rudiments/chapter22/regex.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+)
+
+func main() {
+	needle := "(?i)chocolate"
+	haystack := "Chocolate is my favorite!"
+	match, err := regexp.MatchString(needle, haystack)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(match)
+}
