fc29af8ce0311b3ed088ba9ffa5897beb3385308
ycyoes <ycyoes@163.com>
2020-08-19 12:46:42 +0800

    flag

---
+++ src/go-programming-tour-book/tour/flagTest.go
@@ -0,0 +1,16 @@
+// flagTest
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	var name string
+	flag.StringVar(&name, "name", "Go语言编程之旅", "帮助信息")
+	flag.StringVar(&name, "n", "Go语言编程之旅", "帮助信息")
+	flag.Parse()
+
+	log.Printf("name: %s", name)
+}
