833ab9a6958737ec704153ce113409ea770935f9
ycyoes <ycyoes@163.com>
2020-08-22 20:01:50 +0800

    parse duration

---
+++ src/go-programming-tour-book/tour/cmd/time.go
@@ -0,0 +1,10 @@
+// time.go]
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
---
+++ src/go-programming-tour-book/tour/internal/timer/time.go
@@ -0,0 +1,23 @@
+// time.go
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func GetNowTime() time.Time {
+	return time.Now()
+}
+
+func GetCalculateTime(currentTimer time.Time, d string) (time.Time, error) {
+	duration, err := time.ParseDuration(d)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return currentTimer.Add(duration), nil
+}
+
+func main() {
+	fmt.Println("Hello World!")
+}
